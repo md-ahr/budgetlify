5841b549f19ddd8c007e84c8b4d521d83da26bf9
Abdul Halim <ahr.web.pro@gmail.com>
2026-04-28 16:35:24 +0600

    Add DATABASE_URL support for pgsql

--- config/database.php
+++ config/database.php
@@ -85,16 +85,18 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'driver'   => 'pgsql',
+            'url'      => env('DATABASE_URL'),   // ← ADD THIS LINE
+            'host'     => env('DB_HOST', '127.0.0.1'),
+            'port'     => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
+            'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
+            'charset'  => 'utf8',
+            'prefix'   => '',
             'prefix_indexes' => true,
-            'search_path' => env('DB_SCHEMA', 'public'),
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'search_path'    => 'public',
+            'sslmode'        => 'prefer',
         ],
 
         'sqlsrv' => [
