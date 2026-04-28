db072bfa2470700e7d70f44907eec2dfe18f1f21
Abdul Halim <ahr.web.pro@gmail.com>
2026-04-28 16:14:17 +0600

    update db to postgresql

--- config/database.php
+++ config/database.php
@@ -87,8 +87,6 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
